a9fbb62191f0eeb1af06bd9f202a75bc4b7f98ef
CarlOscarMarcus <oscar@wysarna.se>
2024-04-15 14:46:50 +0200

    Pytest development (capsys not working)

--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -1,33 +1,49 @@
 import pytest
-
-from unittest.mock import patch, MagicMock
+from unittest.mock import MagicMock
 from src.controllers.usercontroller import UserController
 
-
 @pytest.fixture
-def usercontroller():
+def user_controller():
     mock_dao = MagicMock()
     return UserController(dao=mock_dao)
 
-class TestUserController:
-    def test_NoneEmail(self):
-        """ Testing the return of an empty string """
-        with pytest.raises(ValueError):
-            UserController.get_user_by_email(self, "")
+def test_get_user_by_email_valid_email(user_controller):
+    """ Mocking the result of returning a users infomation with only one user connected to one email"""
+    user_controller.dao.find.return_value = [{'email': 'test@example.com', 'name': 'Test User'}]
 
-    def test_invalidEmail(self):
-        """ Testing an email that do not match the emailValidator requirements """
-        with pytest.raises(ValueError):
-            UserController.get_user_by_email(self, "Test")
+    user = user_controller.get_user_by_email('test@example.com')
 
-    def test_validEmailNotUsed(self):
-        """ Testing an email that is valid and not in use """
-        with pytest.raises(Exception):
-            UserController.get_user_by_email(self, 'Test@valid.com')
+    assert user == user_controller.dao.find.return_value[0]
 
-    def test_validEmailUsed(self):
-        """ Testing an email that is valid and not in use """
-       
+def test_get_user_by_email_valid_email_no_user(user_controller):
+    """ Testing get user infomation from an valid email with no users on that email"""
+    with pytest.raises(Exception):
+        user_controller.dao.find.return_value = Exception
+        user_controller.get_user_by_email('test@example.com')
 
 
+def test_get_user_by_email_invalid_email(user_controller):
+    """ Testing get user infomation from an invalid email"""
+    with pytest.raises(ValueError):
+        user_controller.get_user_by_email('invalid_email')
 
+
+def test_get_user_by_email_multiple_users(user_controller, capsys):
+    """ Mocking the result of returning a users infomation with multiple user connected to one email """
+    # Record the diffrent outputs for the console
+    captured = capsys.readouterr()
+
+    user_controller.dao.find.return_value = [{'email': 'test@example.com', 'name': 'Test User 1'},
+                                             {'email': 'test@example.com', 'name': 'Test User 2'}]
+    
+    user = user_controller.get_user_by_email('test@example.com')
+    
+    assert user == user_controller.dao.find.return_value[0]
+    assert captured.out == 'Error: more than one user found with mail test@example.com'
+
+def test_update(user_controller):
+    user_controller.update = MagicMock(return_value=True)
+
+    update_result = user_controller.update(123, {'name': 'Updated Name'})
+
+    assert update_result == True
