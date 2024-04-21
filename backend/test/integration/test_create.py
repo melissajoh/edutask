e9a001da539bdd42b43c46f2f86ef43ca442780f
Melissa Johansson <mejo22@student.bth.se>
2024-04-21 11:52:01 +0200

    possible fixture fix

--- backend/test/integration/test_create.py
+++ backend/test/integration/test_create.py
@@ -1,45 +1,59 @@
 import pytest
 import pymongo
+from unittest.mock import patch
 from src.util.dao import DAO
 
 @pytest.fixture
-def dao():
-    dao = DAO("task")
-    yield dao
-    dao.drop()
+@patch('src.util.dao.getValidator')
+def dao(mock_getValidator):
+    mock_getValidator.return_value = {
+    "$jsonSchema": { "bsonType": "object",
+        "required": ["title", "description"],
+        "properties": {
+            "title": {
+                "bsonType": "string",
+                "description": "the title of a task must be determined",
+                "uniqueItems": True
+            }, 
+            "description": {
+                "bsonType": "string",
+                "description": "the description of a task must be determined"
+            }}}}
+    dao = DAO("test")
+    return dao
 
 def test_create_all_correct(dao):
     """ Test valid data correct properties, bson and uniqueItems, returning object """
-    task_dict = {'title': 'Unique', 'description': 'Description added'}
+    test_dict = {'title': 'Unique', 'description': 'Description added'}
     expected_res = {'_id': {'$oid': ''}, 'title': 'Unique', 'description': 'Description added'}
-    result = dao.create(task_dict)
+    result = dao.create(test_dict)
     assert result.keys() == expected_res.keys()
 
 def test_create_incorrect_properties(dao):
     """ Test valid data with incorrect properties, raising WriteError """
     with pytest.raises(pymongo.errors.WriteError):
-        task_dict = {'title': 'Title'}
-        result = dao.create(task_dict)
+        test_dict = {'title': 'Title'}
+        result = dao.create(test_dict)
         assert result == Exception
 
 def test_create_incorrect_bson(dao):
     """ Test valid data with incorrect bson type, raising WriteError """
     with pytest.raises(pymongo.errors.WriteError):
-        task_dict = {'title': 1, 'description': 'Description added'}
-        result = dao.create(task_dict)
+        test_dict = {'title': 1, 'description': 'Description added'}
+        result = dao.create(test_dict)
         assert result == Exception
 
 def test_create_incorrect_uniqueItems(dao):
     """ Test valid data with incorrect unique items, raising WriteError """
     with pytest.raises(pymongo.errors.WriteError):
-        task_dict = {'title': 'Not unique', 'description': 'Description added'}
-        dao.create(task_dict)
-        result = dao.create(task_dict)
+        test_dict = {'title': 'Not unique', 'description': 'Description added'}
+        dao.create(test_dict)
+        result = dao.create(test_dict)
         assert result == Exception
 
 def test_create_invalid_data(dao):
     """ Test invalid data, raising Exception """
     with pytest.raises(Exception):
-        dict = "invalid" 
-        result = dao.create(dict)
+        test_dict = "invalid" 
+        result = dao.create(test_dict)
         assert result == Exception
