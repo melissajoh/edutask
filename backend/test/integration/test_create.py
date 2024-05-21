8b6c03d6cd0f70357d01f61408f0df53ea3b7f38
unknown <melissa_johansson@hotmail.com>
2024-05-21 11:59:49 +0200

    test fixes

--- backend/test/integration/test_create.py
+++ backend/test/integration/test_create.py
@@ -22,7 +22,6 @@ def dao(mock_getValidator):
     dao = DAO("test")
     return dao
 
-@pytest.mark.integration
 def test_create_all_correct(dao):
     """ Test valid data correct properties, bson and uniqueItems, returning object """
     test_dict = {'title': 'Unique', 'description': 'Description added'}
@@ -30,34 +29,27 @@ def test_create_all_correct(dao):
     result = dao.create(test_dict)
     assert result.keys() == expected_res.keys()
 
-@pytest.mark.integration
 def test_create_incorrect_properties(dao):
     """ Test valid data with incorrect properties, raising WriteError """
     with pytest.raises(pymongo.errors.WriteError):
         test_dict = {'title': 'Title'}
-        result = dao.create(test_dict)
-        assert result == Exception
+        dao.create(test_dict)
 
-@pytest.mark.integration
 def test_create_incorrect_bson(dao):
     """ Test valid data with incorrect bson type, raising WriteError """
     with pytest.raises(pymongo.errors.WriteError):
         test_dict = {'title': 1, 'description': 'Description added'}
-        result = dao.create(test_dict)
-        assert result == Exception
+        dao.create(test_dict)
 
 def test_create_incorrect_uniqueItems(dao):
     """ Test valid data with incorrect unique items, raising WriteError """
     with pytest.raises(pymongo.errors.WriteError):
         test_dict = {'title': 'Not unique', 'description': 'Description added'}
         dao.create(test_dict)
-        result = dao.create(test_dict)
-        assert result == Exception
+        dao.create(test_dict)
 
-@pytest.mark.integration
 def test_create_invalid_data(dao):
     """ Test invalid data, raising Exception """
     with pytest.raises(Exception):
         test_dict = "invalid" 
-        result = dao.create(test_dict)
-        assert result == Exception
+        dao.create(test_dict)
--- backend/test/unit/test_usercontroller.py
+++ backend/test/unit/test_usercontroller.py
@@ -8,7 +8,6 @@ def user_controller():
     mock_dao = MagicMock()
     return UserController(dao=mock_dao)
 
-@pytest.mark.unit
 def test_get_user_by_email_valid_email(user_controller):
     """ Mocking the result of returning a users infomation with only one user connected to one email"""
     user_controller.dao.find.return_value = [{'email': 'test@example.com', 'name': 'Test User'}]
@@ -17,34 +16,38 @@ def test_get_user_by_email_valid_email(user_controller):
 
     assert user == user_controller.dao.find.return_value[0]
 
-@pytest.mark.unit
 def test_get_user_by_email_invalid_email(user_controller):
     """ Testing get user infomation from an invalid email"""
+    user_controller.dao.find.return_value = ValueError
     with pytest.raises(ValueError):
-        user_controller.get_user_by_email('invalid_email')
+        user_controller.get_user_by_email('invalid@email_invalid_')
 
-@pytest.mark.unit
 def test_get_user_by_email_multiple_users(user_controller):
     """ Mocking the result of returning a users infomation with multiple user connected to one email """
     user_controller.dao.find.return_value = [{'email': 'test@example.com', 'name': 'Test User 1'},
                                              {'email': 'test@example.com', 'name': 'Test User 2'}]
 
     with patch('sys.stdout', new_callable=io.StringIO) as stdout_mock:
-            user = user_controller.get_user_by_email('test@example.com')
-            assert user == user_controller.dao.find.return_value[0]
+            user_controller.get_user_by_email('test@example.com')
             assert stdout_mock.getvalue() == 'Error: more than one user found with mail test@example.com\n'
-
-def test_update(user_controller):
-    user_controller.update = MagicMock(return_value=True)
-
-    update_result = user_controller.update(123, {'name': 'Updated Name'})
-
-    assert update_result == True
 
 @pytest.mark.unit
 def test_get_user_by_email_valid_no_user(user_controller):
     """ Testing get user information from valid email with no users """
-    # with pytest.raises(TypeError):
     user_controller.dao.find.return_value = None
     user = user_controller.get_user_by_email('nouser@example.com')
     assert user == None
+
+@pytest.mark.unit
+def test_get_user_by_email_database_failure_valid_email(user_controller):
+     """ Testing failed database """
+     user_controller.dao.find.return_value = Exception
+     with pytest.raises(Exception):
+          user_controller.get_user_by_email("exception@test.com")
+
+@pytest.mark.unit
+def test_get_user_by_email_database_failure_invalid_email(user_controller):
+     """ Testing failed database """
+     user_controller.dao.find.return_value = Exception
+     with pytest.raises(Exception):
+          user_controller.get_user_by_email("exception.test")
